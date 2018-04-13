77f660f841ff481218569b0d88a2f80ad1ff97b1
YangWanjun <ywjsailor@gmail.com>
2018-04-13 16:16:26 +0900

    clear trash

--- areaparking-ubuntu/enterpoint.sh
+++ areaparking-ubuntu/enterpoint.sh
@@ -2,7 +2,7 @@
 if [ ! -d /areaparking ]; then
   mkdir /areaparking
   cd /areaparking
-  git clone https://yangwanjun%40e-business.co.jp:ywjsailor@e-business.co.jp:3000/Areaparking/areaparking.git ./
+  git clone https://e-business.co.jp:3000/Areaparking/areaparking.git ./
 
 fi
 
