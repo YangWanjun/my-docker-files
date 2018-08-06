127704184633d9f4c2b25c72f60caddd5ec1e6a1
YangWanjun <ywjsailor@gmail.com>
2018-08-06 17:35:07 +0900

    git url chnaged

--- areaparking-alpine/enterpoint.sh
+++ areaparking-alpine/enterpoint.sh
@@ -2,7 +2,7 @@
 if [[ ! -d /areaparking ]]; then
   mkdir /areaparking
   cd /areaparking
-  git clone https://git.e-business.co.jp:3000/Areaparking/areaparking.git ./
+  git clone https://git.e-business.co.jp/Areaparking/areaparking.git ./
 
 fi
 
