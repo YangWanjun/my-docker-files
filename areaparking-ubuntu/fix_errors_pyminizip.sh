900afb90c29dceada75ffa12ffdc763d914235c4
YangWanjun <ywjsailor@gmail.com>
2018-10-09 15:00:55 +0900

    remove pyminizip

--- areaparking-ubuntu/fix_errors_pyminizip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tar -xzf /root/pyminizip-0.2.1.tar.gz
-cd /root/pyminizip-0.2.1
-sed -i '29i#include "zlib.h"' src/crypt.h
-python3 setup.py install
-rm -rf /root/pyminizip-0.2.1
-rm /root/pyminizip-0.2.1.tar.gz
