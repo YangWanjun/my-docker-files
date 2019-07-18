1ed4a793e21669d1b2f5ee0480236e416994a19b
YangWanjun <ywjsailor@gmail.com>
2019-07-18 16:45:48 +0900

    intra-mart

---
+++ inter-mart/config_resin.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp /root/postgresql-42.2.6.jar /resin/lib/
--- inter-mart/start_resin.sh
+++ inter-mart/start_resin.sh
@@ -3,7 +3,7 @@
 tar xzf resin-pro-4.0.61.tar.gz
 cd /root/resin-pro-4.0.61
 
-mkdir /imart
-./configure --prefix=</imart> --enable-64bit
+mkdir /resin
+./configure --prefix='/resin' --enable-64bit
 make
 make install
