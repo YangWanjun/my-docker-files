35bee19d366fcfa9a807f5eb1d74309e2051b4c2
Yang Wanjun <ywjsailor@gmail.com>
2019-09-30 19:54:47 +0900

    openoffice

--- openoffice/install_openoffice.sh
+++ openoffice/install_openoffice.sh
@@ -2,9 +2,9 @@
 
 # download openoffice3.3.0
 cd /root
-cat /root/bin/OOo_3.3.0_Linux_x86-64_install-deb_ja.tar.gz.part* >openoffice.tar.gz
+cat /root/bin/Apache_OpenOffice_4.1.7_Linux_x86-64_install-deb_ja.tar.gz.part* >openoffice.tar.gz
 tar xvf openoffice.tar.gz
 # install
-cd OOO330_m20_native_packed-1_ja.9567/DEBS/
+cd ja/DEBS/
 dpkg -i *.deb
 update-alternatives --install /usr/bin/soffice soffice /opt/openoffice.org3/program/soffice 1
