ad3e25193f9ca4d93ac65fffb46461a29fa36aad
Ryan Yang <ywjsailor@gmail.com>
2023-05-28 22:03:52 +0900

    sales

---
+++ sales/01.patch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sed -i 's/max_length=1024/max_length=512/g' /usr/local/lib/python3.11/site-packages/django_cas_ng/migrations/0002_auto_20201023_1400.py
+sed -i 's/max_length=1024/max_length=512/g' /usr/local/lib/python3.11/site-packages/django_cas_ng/migrations/0003_auto_20210813_1226.py
