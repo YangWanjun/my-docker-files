86820fdb57f070462697a3678e0bba0b535a9b34
Ryan Yang <ywjsailor@gmail.com>
2023-08-17 23:57:34 +0900

    sales

--- sales/01.patch.sh
+++ sales/01.patch.sh
@@ -2,3 +2,4 @@
 
 sed -i 's/max_length=1024/max_length=512/g' /usr/local/lib/python3.11/site-packages/django_cas_ng/migrations/0002_auto_20201023_1400.py
 sed -i 's/max_length=1024/max_length=512/g' /usr/local/lib/python3.11/site-packages/django_cas_ng/migrations/0003_auto_20210813_1226.py
+# 改行はLFでなければならない
